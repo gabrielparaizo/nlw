3ccf3def96515ad01ca12d8d73c03fe8569f9f13
Gabriel Lopes <gabriellopees@hotmail.com>
2020-10-14 00:37:39 -0300

    nlw3.0

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+// Import React, this line should always be here
+import './styles/global.css';
+import React from 'react';
+
+import Routes from './routes';
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
